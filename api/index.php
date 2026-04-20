31e6b4ae16cd05c882b456c34afcf4f03d05e260
Steven Gerrard <ytbreven@gmail.com>
2026-04-20 14:30:44 +0700

    Solusi Hak Akses Storage Vercel (FIX 500 error)

--- api/index.php
+++ api/index.php
@@ -1,4 +1,26 @@
 <?php
 
-// Forward Laravel Requests for Vercel
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Mempersiapkan struktur wadah (Writable /tmp di Vercel)
+$compiledViewPath = '/tmp/storage/framework/views';
+$cachePath = '/tmp/storage/framework/cache/data';
+$logsPath = '/tmp/storage/logs';
+
+foreach ([$compiledViewPath, $cachePath, $logsPath] as $path) {
+    if (!is_dir($path)) {
+        mkdir($path, 0777, true);
+    }
+}
+
+// Muat komponen Laravel
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Paksa Laravel menggunakan wadah Serverless
+$app->useStoragePath('/tmp/storage');
+
+// Proses jalankan permintaan
+$app->handleRequest(Request::capture());
