ac5c4d5c235985cb8d42d1c8416e0194d193f34c
Steven Gerrard <ytbreven@gmail.com>
2026-04-20 14:49:41 +0700

    cleanup : menghilangkan teks debug pelacakan vercel

--- api/index.php
+++ api/index.php
@@ -4,16 +4,11 @@
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
     $_SERVER['HTTPS'] = 'on';
 }
-ini_set('display_errors', '1');
-error_reporting(E_ALL);
-
-echo "Step 1: PHP Jalan. <br>";
 
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-echo "Step 2: Menyiapkan tmp. <br>";
 $compiledViewPath = '/tmp/storage/framework/views';
 $cachePath = '/tmp/storage/framework/cache/data';
 $logsPath = '/tmp/storage/logs';
@@ -25,21 +20,12 @@ foreach ([$compiledViewPath, $cachePath, $logsPath, $bootstrapPath] as $path) {
     }
 }
 
-echo "Step 3: Memuat Vendor. <br>";
-if (!file_exists(__DIR__.'/../vendor/autoload.php')) {
-    die("VENDOR TIDAK DITEMUKAN! Vercel gagal menjalankan composer install.");
-}
 require __DIR__.'/../vendor/autoload.php';
-
-echo "Step 4: Memuat Bootstrap Laravel 11. <br>";
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-echo "Step 5: Overriding Storage Path. <br>";
 $app->useStoragePath('/tmp/storage');
 if (method_exists($app, 'useBootstrapPath')) {
     $app->useBootstrapPath('/tmp/bootstrap');
 }
 
-echo "Step 6: Handle Request. <br>";
 $app->handleRequest(Request::capture());
-
